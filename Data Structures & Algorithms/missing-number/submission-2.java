35c929fad26e2b650b5a0d3390126c1b88fe0754
Giacomo D'Alessandro <gdalessandro@wisc.edu>
2026-07-20 22:39:19 -0500

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int n : nums) {
+            set.add(n);
+        }
+
+        for (int i = 0; i < nums.length + 1; i++) {
+            if (!set.contains(i)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
