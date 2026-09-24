cd8cb4c8fd05ff5853971c8a382e215455bb1dab
Giacomo D'Alessandro <gdalessandro@wisc.edu>
2026-09-24 10:06:30 -0500

    Add: validate-parentheses - submission-13

---
+++ Data Structures & Algorithms/validate-parentheses/submission-13.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        brackets = {")" : "(", "]" : "[", "}" : "{"}
+        
+
+        for c in s:
+            if c in brackets:
+                if not stack or stack[-1] != brackets[c]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(c)
+
+                
+        return not stack
